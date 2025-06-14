8a5f3061c60da52cb7d4e309a4c1b030cf7fedca
Artsiom Shymanouski <artsiom.shymanouski@leverx.com>
2025-06-14 18:58:29 +0300

    feat: add Github Actions workflow

--- terraform/task-1/backend.tf
+++ terraform/task-1/backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = "~> 1.12.0"
 
   backend "s3" {
-    bucket         = "aws-devops-2025q2-shymanouski-terraform-states"
-    key            = "task-1/terraform.tfstate"
-    region         = "us-east-2"
-    use_lockfile   = true
+    bucket       = "aws-devops-2025q2-shymanouski-terraform-states"
+    key          = "task-1/terraform.tfstate"
+    region       = "us-east-2"
+    use_lockfile = true
   }
 } 
