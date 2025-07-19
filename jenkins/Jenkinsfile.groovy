d9b1187495480dc6e6131a95d9b157ef8540a941
Artsiom Shymanouski <artsiom.shymanouski@leverx.com>
2025-07-19 14:40:27 +0300

    feat: add github webhook

--- jenkins/Jenkinsfile.groovy
+++ jenkins/Jenkinsfile.groovy
@@ -37,6 +37,10 @@ pipeline {
         skipDefaultCheckout(true)
     }
     
+    triggers {
+        githubPush()
+    }
+    
     environment {
         AWS_ACCOUNT_ID = credentials('aws-account-id')
         AWS_REGION = 'us-east-1'
