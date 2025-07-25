faefbcf970fc6ac7c372441cfb2d393bf367c3ad
Artsiom Shymanouski <artsiom.shymanouski@leverx.com>
2025-07-26 02:34:30 +0300

    feat: force jenkins use grafana variable file

--- jenkins/Jenkinsfile.monitoring.groovy
+++ jenkins/Jenkinsfile.monitoring.groovy
@@ -138,16 +138,9 @@ pipeline {
                                 helm upgrade --install grafana oci://registry-1.docker.io/bitnamicharts/grafana \\
                                     --namespace monitoring \\
                                     --create-namespace \\
+                                    --values helm/monitoring/grafana/values.yaml \\
                                     --set admin.user=${GRAFANA_USER} \\
-                                    --set admin.password=${GRAFANA_PASSWORD} \\
-                                    --set service.type=NodePort \\
-                                    --set service.nodePorts.grafana=32003 \\
-                                    --set persistence.enabled=true \\
-                                    --set persistence.size=8Gi \\
-                                    --set resources.requests.memory=256Mi \\
-                                    --set resources.requests.cpu=100m \\
-                                    --set resources.limits.memory=512Mi \\
-                                    --set resources.limits.cpu=500m
+                                    --set admin.password=${GRAFANA_PASSWORD}
                             """
                         }
 
