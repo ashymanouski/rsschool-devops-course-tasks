429ed127e45914f799119194ca6d279bada8fb68
Artsiom Shymanouski <artsiom.shymanouski@leverx.com>
2025-07-26 14:11:30 +0300

    feat: add dashboard CM for Grafana

--- jenkins/Jenkinsfile.monitoring.groovy
+++ jenkins/Jenkinsfile.monitoring.groovy
@@ -168,7 +168,9 @@ pipeline {
                     script {
                         echo "Deploying Grafana dashboards..."
                         
-                        sh "kubectl apply -f helm/monitoring/grafana/templates/dashboards/"
+                        // sh "kubectl apply -f helm/monitoring/grafana/templates/dashboards/"
+                        
+                        sh "kubectl create configmap cluster-monitoring-dashboard --from-file=cluster-monitoring.json=helm/monitoring/grafana/dashboards/cluster-monitoring.json -n monitoring"
                         
                         echo "Dashboards deployed successfully"
                     }
